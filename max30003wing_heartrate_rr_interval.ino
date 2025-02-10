9eb5ff810a7c6dc2a1ecd9fbe616dcfddac84dd2
mdabdurrahman <marahman.iut@gmail.com>
2025-02-10 23:59:04 +0200

    Update max30003wing_heartrate_rr_interval.ino

--- max30003wing_heartrate_rr_interval.ino
+++ max30003wing_heartrate_rr_interval.ino
@@ -14,7 +14,7 @@
 //  | GND              | Digital Gnd          |  GND             |
 //	| INTB             | Interrupt            |  D3              |
 //
-//  Copyright (c) Md Abdur Rahman
+//  Copyright Ⓒ Md Abdur Rahman
 //
 /////////////////////////////////////////////////////////////////////////////////////////
 
