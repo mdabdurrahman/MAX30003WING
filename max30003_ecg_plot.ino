23d8a005b4129a507f3a984c07e2080047e500f3
mdabdurrahman <marahman.iut@gmail.com>
2025-02-10 23:54:01 +0200

    Update max30003_ecg_plot.ino

--- max30003_ecg_plot.ino
+++ max30003_ecg_plot.ino
@@ -18,15 +18,6 @@
 //  | INT1             | Interrupt1           |  02              |
 //  | INT2             | Interrupt2           |  -               |
 //
-//   This software is licensed under the MIT License(http://opensource.org/licenses/MIT).
-//
-//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-//   NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-//   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-//   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-//   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//
 /////////////////////////////////////////////////////////////////////////////////////////
 
 #include <SPI.h>
